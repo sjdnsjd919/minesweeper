d96c107f10267cb9515136ffd6123a7f57f57588
sjdnsjd919 <chauhm1@kgv.hk>
2022-07-30 17:43:51 +0800

    Update minesweeper1.py

--- minesweeper1.py
+++ minesweeper1.py
@@ -1,4 +1,12 @@
-for i in range(0,9):
-    for j in range(0,9):
-        map[i][j]=random.choices([0,1]], weights=[0.85,0.15], k=1)
-print(map)
+import random
+
+dim1, dim2 = (10, 10)
+map = [[random.choices([0,1], weights=[.7937,.2063], k=1) for i in range(dim1)] for j in range(dim2)]
+map[0][0]=[1]
+for i in range(dim1):
+    print("% s" % str(map[i]))
+def val(x,y):
+    return [i for i in range(8) if check(x,y,i)]
+def check(x,y,i):
+    return 1 if i == 0 and map[x-1][y-1] == 1 else None
+print(check(1,1,0))
